846ee65e264a844be42950f956c95e6cd249b3fc
jnrsai24 <jnrsai24@gmail.com>
2023-07-04 15:30:13 +0000

    input value relate error in console

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,18 @@
 // Write your JS code here
+import './index.css'
+
+const Header = () => (
+  <nav className="header-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+      alt="website logo"
+    />
+    <ul className="nav-items">
+      <li>Home</li>
+      <li>Product</li>
+      <li>Cart</li>
+      <button type="button">Logout</button>
+    </ul>
+  </nav>
+)
+export default Header
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,1 +1,136 @@
 // Write your JS code here
+import {Component} from 'react'
+import './index.css'
+
+class LoginForm extends Component {
+  state = {username: '', password: ''}
+
+  submitLogin = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+
+    const userDetails = {username, password}
+
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  onPassWord = event => {
+    this.setState({password: event.target.value})
+  }
+
+  passWord = () => {
+    // const {password} = this.state
+    // return (
+    //   <div className="inputEl-container">
+    //     <label htmlFor="passwordLabel">PASSWORD</label>
+    //     <input
+    //       id="passwordLabel"
+    //       type="password"
+    //       placeholder="Password"
+    //       value={password}
+    //       onChange={this.onPassWord}
+    //     />
+    //   </div>
+    // )
+
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  onUserName = event => {
+    this.setState({username: event.target.value})
+  }
+
+  userName = () => {
+    // const {username} = this.state
+    // return (
+    //   <div className="inputEl-container">
+    //     <label htmlFor="usernameLable">USERNAME</label>
+    //     <input
+    //       id="usernameLable"
+    //       type="text"
+    //       placeholder="Username"
+    //       value={username}
+    //       onChange={this.onUsername}
+    //       //
+    //     />
+    //   </div>
+    // )
+
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-filed"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  loginForm = () => (
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+        alt="website logo"
+        className="logo-img"
+      />
+      <div>{this.userName()}</div>
+      <div>{this.passWord()}</div>
+    </div>
+  )
+
+  render() {
+    return (
+      <div className="login-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          alt="website login"
+          className="login-img"
+        />
+        <form onSubmit={this.submitLogin}>
+          {/* {this.loginForm()} */}
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+            className="logo-img"
+          />
+          <div>{this.userName()}</div>
+          <div>{this.passWord()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
